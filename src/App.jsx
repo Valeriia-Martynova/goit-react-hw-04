fc47a03273ca5d7d4835f9bb9594d0872e3df5c3
Valeriia Martynova <vmartynova95@gmail.com>
2025-01-24 21:22:31 +0200

    create project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Image search service</h1>
+    </div>
+  );
+};
+
+export default App;
